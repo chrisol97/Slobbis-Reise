e0ddbc9d53d30a43e88634f04c4c421b21b3cda9
chrisol97 <166822669+chrisol97@users.noreply.github.com>
2024-04-17 12:00:17 +0200

    Add files via upload

--- barneprat/actions/dragNdrop.mjs
+++ barneprat/actions/dragNdrop.mjs
@@ -1,4 +1,6 @@
 
+import { isRuningOnTouchDevice, randomString } from "../utils/utils.mjs"
+
 let dragTarget = null;
 let dragItem = null;
 let shiftX = 0;
@@ -6,7 +8,37 @@ let shiftY = 0;
 
 const dropTargets = []
 
-window.document.onmousemove = (e) => {
+function touchStart(e) {
+    //e.preventDefault(); // This makes the click events stop working.
+
+    let target = e.target;
+    console.log(target);
+
+    let dragId = target.getAttribute("data-drag-id");
+    if (dragId == undefined) { return }
+
+    const touch = e.touches[0];
+    const moveOffsetX = target.offsetLeft - touch.pageX;
+    const moveOffsetY = target.offsetTop - touch.pageY;
+
+    target.addEventListener("touchmove", touchMove, { passive: false });
+
+    function touchMove(e) {
+        const touch = e.touches[0];
+        const positionX = touch.pageX + moveOffsetX;
+        const positionY = touch.pageY + moveOffsetY;
+
+        target.style.left = `${positionX}px`;
+        target.style.top = `${positionY}px`;
+    }
+}
+
+if (isRuningOnTouchDevice()) {
+    document.body.addEventListener('touchstart', touchStart, { passive: false });
+}
+
+
+const onMovement = (e) => {
     if (dragTarget) {
         dragTarget.style.left = (e.pageX - shiftX) + "px";
         dragTarget.style.top = (e.pageY - shiftY) + "px";
@@ -15,7 +47,7 @@ window.document.onmousemove = (e) => {
     }
 }
 
-window.document.onmouseup = (e) => {
+const endMovment = (e) => {
 
     if (!dragTarget) { return }
 
@@ -31,7 +63,6 @@ window.document.onmouseup = (e) => {
             dropTarget = dropTarget[0];
             dropTarget.callback(dragItem);
         }
-
     }
 
 
@@ -39,8 +70,13 @@ window.document.onmouseup = (e) => {
     dragItem = null;
 }
 
+window.document.onmouseup = endMovment;
+window.document.onmousemove = onMovement;
 
 const Drag = (item, label = "dragDefault") => {
+
+    item.dragId = randomString();
+    item.target.setAttribute("data-drag-id", item.dragId);
 
     item.target.onmousedown = (e) => {
         dragItem = item;
